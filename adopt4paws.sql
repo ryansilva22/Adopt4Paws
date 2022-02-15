cf5415699eb1b89d08db98b037cacc9861293721
Laceration <74409188+Lacerationz@users.noreply.github.com>
2022-02-15 02:13:04 +0000

    Add files via upload

---
+++ adopt4paws.sql
@@ -0,0 +1,31 @@
+create table usuario (
+id_usuario int NOT NULL AUTO_INCREMENT,
+nome varchar(50) NOT NULL,
+email varchar(50) NOT NULL,
+morada varchar(50) NOT NULL,
+idade tinyint NOT NULL,
+telefone varchar(15) NOT NULL,
+sexo varchar(10) NOT NULL,
+nome_usuario varchar(20) NOT NULL,
+senha varchar(25) NOT NULL,
+PRIMARY KEY(id_usuario)
+);
+
+
+create table animal(
+id_animal int NOT NULL AUTO_INCREMENT,
+tipo varchar(15) NOT NULL,
+raca varchar(15) NOT NULL,
+cor varchar(15) NOT NULL,
+idade tinyint NOT NULL,
+sexo varchar(10) NOT NULL,
+localidade varchar (20) NOT NULL,
+PRIMARY KEY (id_animal)
+);
+
+create table organização(
+id_organizacao int NOT NULL AUTO_INCREMENT,
+nome_organizacao varchar(15) NOT NULL,
+localidade_organizacao varchar(20) NOT NULL,
+PRIMARY KEY (id_organizacao)
+);
